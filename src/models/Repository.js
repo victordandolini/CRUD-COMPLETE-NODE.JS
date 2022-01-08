42ef02568da3bbdf6b1351ceb391ceef50cc4e49
Victor Dandolini <victordandolini@hotmail.com>
2022-01-08 00:32:28 -0300

    CRUD NODE.JS

---
+++ src/models/Repository.js
@@ -0,0 +1,26 @@
+import  mongoose  from "mongoose";
+
+const repositorySchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            
+        },
+        url: {
+            type: String,
+            required: true,
+            unique: true
+        },
+        userId: {
+            type: String,
+            required: true,
+        }
+    },
+    {
+        timestamps: true
+    }
+
+);
+
+export default mongoose.model('repository', repositorySchema);
